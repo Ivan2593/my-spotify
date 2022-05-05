7cb3074b8a86ccbbdbf8d6230a456aae41f24ca9
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-05-05 16:46:47 +0700

    верстка

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from "./Home";
-import Album from "./Album";
 
 // @ts-ignore
 // @ts-ignore
 ReactDOM.render(
   <React.StrictMode>
       <Home/>
-      {/*<Album/>*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
